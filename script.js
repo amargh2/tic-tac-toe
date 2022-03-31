1fe8b3c96a9db444c77f5085a09d65c86c8bc707
amargh2 <margherioa@gmail.com>
2022-03-30 22:28:59 -0500

    major refactor of what was mostly a mess of hard coded variables -- iterated through the game map according to data id instead. faster/better

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 const game = (function () {
+    
+    //board with values, used to evaluate win conditions
+
     const board = {
         box0:' ',
         box1:' ',
@@ -12,180 +15,95 @@ const game = (function () {
         box8:' ',
     }
     
+    //header manipulation
     header = document.querySelector('.header')
-
+    //checks for win and publishes winner to the header
     function checkForXWin(board) {
         if (board.box0 =='x' && board.box1 =='x' && board.box2 =='x') {
-            header.textContent += 'x wins'
+            header.textContent = 'x wins'
         }
         if (board.box0 =='x' && board.box4 =='x' && board.box8 =='x') {
-            console.log('x wins')
+            header.textContent = 'X wins'
         }
         if (board.box3 =='x' && board.box4 =='x' && board.box5 =='x') {
-        console.log('x wins')
+            header.textContent = 'X wins'
         }
         if (board.box6 =='x' && board.box7 =='x' && board.box8 =='x') {
-            console.log('x wins')
+            header.textContent = 'X wins'
+        }
+        if (board.box2 =='x' && board.box5 =='x' && board.box8 =='x') {
+            header.textContent = 'X wins'
+        }
+        if (board.box1 =='x' && board.box4 =='x' && board.box7 =='x') {
+            header.textContent = 'X wins'
+        }
+        if (board.box0 =='x' && board.box3 =='x' && board.box6 =='x') {
+            header.textContent = 'X wins'
         }    
     }
     
     function checkForOWin(board) {
         if (board.box0 =='o' && board.box1 =='o' && board.box2 =='o') {
-        console.log('o wins')
+            header.textContent = 'O wins'
         }
         if (board.box0 =='o' && board.box4 =='o' && board.box8 =='o') {
-            console.log('o wins')
+            header.textContent = 'O wins'
         }
         if (board.box3 =='o' && board.box4 =='o' && board.box5 =='o') {
-        console.log('o wins')
+            header.textContent = 'O wins'
         }
         if (board.box6 =='o' && board.box7 =='o' && board.box8 =='o') {
-            console.log('o wins')
+            header.textContent = 'O wins'
         }
-    }
-    
-    const divMappings = {
-        div0: document.querySelector("[data-id='0']"),
-        div1: document.querySelector("[data-id='1']"),
-        div2: document.querySelector("[data-id='2']"),
-        div3: document.querySelector("[data-id='3']"),
-        div4: document.querySelector("[data-id='4']"),
-        div5: document.querySelector("[data-id='5']"),
-        div6: document.querySelector("[data-id='6']"),
-        div7: document.querySelector("[data-id='7']"),
-        div8: document.querySelector("[data-id='8']"),
+        if (board.box2 =='o' && board.box5 =='o' && board.box8 =='o') {
+            header.textContent = 'O wins'
+        }
+        if (board.box1 =='o' && board.box4 =='o' && board.box7 =='o') {
+            header.textContent = 'O wins'
+        }
+        if (board.box0 =='o' && board.box3 =='o' && board.box6 =='o') {
+            header.textContent = 'O wins'
+        }
+        
     }
 
+    board.box0.indexOf()
     //this is ugly -- i admit it; but it felt like the easiest way at the time. will look into how to refactor/simplify this eventually. just wanted something working.
-    //this is the click event listener that alters the game board as welll as the board value
+    //this is the click event listener that alters the game board as well as the board value
     gameSpace = document.querySelector('.gamespace')
     gameSpace.addEventListener('click', function(e){
         if (e.target.classList.contains('x')) {
             data = e.target.parentElement.dataset.id
-            if (data == 0) {
-                div = divMappings.div0
-                div.textContent = 'X'
-                board.box0='x'
-                checkForXWin(board)
-                checkForOWin(board)
-                console.log(board) 
-            }
-            if (data == 1) {
-                div = divMappings.div1
-                div.textContent = 'X'
-                board.box1 = 'x'
-                checkForXWin(board)
-                checkForOWin(board)
-                console.log(board) 
-
-            }
-            if (data == 2) {
-                div = divMappings.div2
-                div.textContent = 'X'
-                board.box2 = 'x'
-                checkForXWin(board)
-                checkForOWin(board)
-                console.log(board) 
-
-            }
-            if (data == 3) {
-                div = divMappings.div3
-                div.textContent = 'X'
-                board.box3 = 'x'
-                checkForXWin(board)
-                checkForOWin(board)
-                console.log(board) 
-
-            }
-            if (data==4) {
-                div=divMappings.div4
-                div.textContent = 'X'
-                board.box4 = 'x'
-                checkForXWin(board)
-                checkForOWin(board)
-                console.log(board) 
-
-            }
-            if (data == 5) {
-                div = divMappings.div5
-                div.textContent = 'X'
-                board.box5 = 'x'
-                checkForXWin(board)
-                checkForOWin(board)
-            }
-            if (data == 6) {
-                div = divMappings.div6
-                div.textContent = 'X'
-                board.box6 ='x'
-                checkForXWin(board)
-                checkForOWin(board)
-            }
-            if (data == 7) {
-                div = divMappings.div7
-                div.textContent = 'X'
-                board.box7 = 'x'
-                checkForXWin(board)
-                checkForOWin(board)
-            }
-            if (data == 8) {
-                div = divMappings.div8
-                div.textContent = 'X'
-                board.box8 = 'x'
-                checkForXWin(board)
-                checkForOWin(board)
+            div = e.target.parentElement
+            div.textContent = 'X'
+            for (element in board) {
+                if (data == element[3]) {
+                    board[element] = 'x'
+                    console.log(board.element)
+                    
+                }
             }
         }
-        if (e.target.classList.contains('o')) {
+        else if (e.target.classList.contains('o')) {
             data = e.target.parentElement.dataset.id
-            if (data == 0) {
-                div = divMappings.div0
-                div.textContent = 'O'
-                board.box0 ='o'
-            }
-            if (data == 1) {
-                div = divMappings.div1
-                div.textContent = 'O'
-                board.box1 = 'o'
-            }
-            if (data == 2) {
-                div = divMappings.div2
-                div.textContent = 'O'
-                board.box2= 'o'
-            }
-            if (data == 3) {
-                div = divMappings.div3
-                div.textContent = 'O'
-                board.box3 = 'o'
-            }
-            if (data==4) {
-                div=divMappings.div4
-                div.textContent = 'O'
-                board.box4 = 'o'
-            }
-            if (data == 5) {
-                div = divMappings.div5
-                div.textContent = 'O'
-                board.box5 = 'o'
-            }
-            if (data == 6) {
-                div = divMappings.div6
-                div.textContent = 'O'
-                board.box6 = 'o'
-            }
-            if (data == 7) {
-                div = divMappings.div7
-                div.textContent = 'O'
-                board.box7 ='o'
-            }
-            if (data == 8) {
-                div = divMappings.div8
-                div.textContent = 'O'
-                board.box8 = 'o'
+            div = e.target.parentElement
+            div.textContent = 'O'
+            for (element in board) {
+                if (data == element[3]) {
+                    board[element] = 'o'
+                    console.log(board.element)
+                }
             }
         }
+        
+        checkForXWin(board)
+        checkForOWin(board)
     })
+
 }())
 
 //player factory function
 
-
+/*const player = (name) {
+    return name
+} */
